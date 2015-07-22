2db81bc5c60cb05a5b44fcb84ae18d309e513983
Kimiebi Akah <kjakah08@gmail.com>
2015-07-22 19:58:54 -0400

    First Commit

---
+++ test/VendingMachineTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by KJA on 7/22/2015.
+ */
+public class VendingMachineTest {
+    @Test
+    public void testJunitBasic() {
+        FizzBuzz ok = new FizzBuzz();
+        assertEquals("1", ok.test("1"));
+    }
+
+
+}
